4de64557c3cff5e26af2dd32a91ea6733e1a514a
Beka <zalgasovbeksultan1@gmail.com>
2025-05-02 10:17:25 +0500

    init

---
+++ src/main/java/kz/lab/valorant_stats_backend/ValorantStatsBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.lab.valorant_stats_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantStatsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorantStatsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/lab/valorant_stats_backend/ValorantStatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.lab.valorant_stats_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantStatsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
